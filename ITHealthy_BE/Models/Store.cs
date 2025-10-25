6bcfb73064ca26298da3655157be29018f7e77ab
TLHSinh <trinhlehongsinh04@gmail.com>
2025-10-25 23:40:39 +0700

    class store

--- ITHealthy_BE/Models/Store.cs
+++ ITHealthy_BE/Models/Store.cs
@@ -11,7 +11,25 @@ public partial class Store
 
     public string? Phone { get; set; }
 
-    public string? AddressStore { get; set; }
+    // Địa chỉ chi tiết
+    public string StreetAddress { get; set; } = null!;
+
+    public string? Ward { get; set; }
+
+    public string? District { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Country { get; set; } = "Việt Nam";
+
+    public string? Postcode { get; set; }
+
+    // Tọa độ bản đồ
+    public decimal? Latitude { get; set; }
+
+    public decimal? Longitude { get; set; }
+
+    public string? GooglePlaceId { get; set; }
 
     public decimal? Rating { get; set; }
 
