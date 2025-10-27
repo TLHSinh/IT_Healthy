8dd0263364347be4363c47a6433467820f5dfe56
TLHSinh <trinhlehongsinh04@gmail.com>
2025-10-27 23:41:39 +0700

    Giao dien ngươi dung - navbar

---
+++ ITHealthy_BE/DTOs/CategorieIngDTO.cs
@@ -0,0 +1,11 @@
+namespace ITHealthy.DTOs
+{
+    public class CategoryIngDTO
+    {
+        public int CategoriesIngredientsId { get; set; }
+
+        public string CategoryName { get; set; } = null!;
+
+        public string? DescriptionCat { get; set; }
+    }
+}
---
+++ ithealthy-fe/src/pages/AboutusPage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-50 relative">
+      <h1 className="text-3xl font-bold text-gray-700">About Us</h1>
+    </div>
+  );
+}
